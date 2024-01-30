5000de2970f44a34e4e9ed461c3b7f7964ceed9d
Flavio <fjcf@poli.br>
2024-01-30 06:59:24 -0300

    feat: Create category entity

--- Codeflix.Catalog.Domain/Entity/Category.cs
+++ Codeflix.Catalog.Domain/Entity/Category.cs
@@ -2,16 +2,19 @@
 {
     public class Category
     {
- 
-        public string Name { get; set; }
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public bool IsActive { get; private set; }
+        public DateTime CreatedAt { get; private set; }
 
-        public string Description { get; set; }
-
-
-        public Category(string name, string description)
+        public Category(string name, string description, bool isActive = true)
         {
+            Id = Guid.NewGuid();
             Name = name;
             Description = description;
+            IsActive = isActive;
+            CreatedAt = DateTime.Now;
         }
     }
 }
