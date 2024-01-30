af96f8756e0b49b62784c7e5e96555e15b4c5a24
Flavio <fjcf@poli.br>
2024-01-30 06:42:20 -0300

    Add project files.

---
+++ Codeflix.Catalog.UnitTests/Domain/Entity/Category/CategoryTest.cs
@@ -0,0 +1,32 @@
+﻿using DomainEntity = Codeflix.Catalog.Domain.Entity;
+
+namespace Codeflix.Catalog.UnitTests.Domain.Entity.Category
+{
+    public class CategoryTest
+    {
+        [Fact(DisplayName = nameof(Instantiate))]
+        [Trait("Domain", "Category - Aggregates")]
+        public void Instantiate()
+        {
+            //Arrange
+            var validData = new
+            {
+                Name = "category name",
+                Description = "Category description"
+            };
+
+
+            //Act
+            var category = new DomainEntity.Category(validData.Name, validData.Description);
+            var dateTimeAfter = DateTime.Now;
+
+
+            //Assert
+            Assert.NotNull(category);
+            Assert.Equal(validData.Name, category.Name);
+            Assert.Equal(validData.Description, category.Description);
+            Assert.NotEmpty(default(Guid), category.Id);
+            Assert.NotEmpty(default(DateTime), category.CreatedAt);
+        }
+    }
+}
