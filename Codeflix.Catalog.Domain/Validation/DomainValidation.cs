17962a4d43eb9e363d8ee20eb0a8bedc2869fdae
Flavio <fjcf@poli.br>
2024-01-30 21:41:32 -0300

    feat: Create Domain Validation

---
+++ Codeflix.Catalog.Domain/Validation/DomainValidation.cs
@@ -0,0 +1,13 @@
+﻿using Codeflix.Catalog.Domain.Exceptions;
+
+namespace Codeflix.Catalog.Domain.Validation
+{
+    public class DomainValidation
+    {
+        public static void NotNull(object target, string fieldName)
+        {
+            if (target is null)
+                throw new EntityValidationException($"{fieldName} should not be null");
+        }
+    }
+}
