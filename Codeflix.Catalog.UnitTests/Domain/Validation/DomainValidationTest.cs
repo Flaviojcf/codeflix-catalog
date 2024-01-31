520882e656d35a4a22707cc5eda1082dc7ae34b9
Flavio <fjcf@poli.br>
2024-01-30 21:41:21 -0300

    test: Create NotNullOk and NotNullThrowWhenNull domain validation test

---
+++ Codeflix.Catalog.UnitTests/Domain/Validation/DomainValidationTest.cs
@@ -0,0 +1,43 @@
+﻿using Bogus;
+using Codeflix.Catalog.Domain.Exceptions;
+using Codeflix.Catalog.Domain.Validation;
+using FluentAssertions;
+
+namespace Codeflix.Catalog.UnitTests.Domain.Validation
+{
+
+    public class DomainValidationTest
+    {
+        private Faker Faker { get; set; } = new Faker();
+
+
+        [Fact(DisplayName = nameof(NotNullOk))]
+        [Trait("Domain", "DomainValidation - Validation")]
+        public void NotNullOk()
+        {
+            //Arrange
+            var value = Faker.Commerce.ProductName();
+
+            //Assert
+            Action action = () => DomainValidation.NotNull(value, "Value");
+
+            //Act
+            action.Should().NotThrow();
+        }
+
+        [Fact(DisplayName = nameof(NotNullThrowWhenNull))]
+        [Trait("Domain", "DomainValidation - Validation")]
+        public void NotNullThrowWhenNull()
+        {
+            //Arrange
+            string value = null;
+
+            //Assert
+            Action action = () => DomainValidation.NotNull(value, "FieldName");
+
+            //Act
+            action.Should().Throw<EntityValidationException>().WithMessage("FieldName should not be null");
+        }
+
+    }
+}
